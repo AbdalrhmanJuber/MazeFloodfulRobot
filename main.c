bd2bb5073e28ea6df4a07ff98518f5d80e62c37c
AbdalrhmanJuber <131310321+AbdalrhmanJuber@users.noreply.github.com>
2024-12-01 20:28:14 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "solver.h"
+#include "API.h"
+
+
+int main(int argc, char* argv[]) {
+    debug_log("Running...");
+    initialize();
+    while (1) {
+        Action nextMove = solver();
+
+        // displays distances on the squares in the simulator
+        for (int x = 0; x < MAZE_SIZE; ++x) {
+            for (int y = 0; y < MAZE_SIZE; ++y) {
+                API_setText(x, y, distances[x][y]);
+            }
+        }
+        
+        switch(nextMove){
+            case FORWARD:
+                API_moveForward();
+                break;
+            case LEFT:
+                API_turnLeft();
+                break;
+            case RIGHT:
+                API_turnRight();
+                break;
+            case IDLE:
+                break;
+        }
+    }
+}
